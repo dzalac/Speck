3e4836914de1945269131ff50e4d2c942071efad
dzalac <dzalac@foi.hr>
2022-04-28 18:14:31 +0200

    28.4.

---
+++ JSlottery/modules/player.js
@@ -0,0 +1,13 @@
+export default class Player {
+
+    constructor(name, surname, lotteryNumbers){
+        this.name=name;
+        this.surname=surname;
+        this.lotteryNumbers=lotteryNumbers;
+
+    }
+
+    getPlayerDetails(){
+        return `${this.name} ${this.surname} [${this.lotteryNumbers}]`;
+    }
+}
